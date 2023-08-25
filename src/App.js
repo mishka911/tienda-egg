d42e19ba6eef8a2b38a21663a5962cc5a06b529f
Vanessa Proaño <vaneproano91@gmail.com>
2023-08-24 20:18:12 -0500

    Iniciando mi repositorio de la tienda egg con react js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import NavMenu from './components/header/NavMenu';
+import Store from './components/header/home/Store';
+import Footer2 from './components/header/home/Footer2';
+import Hero from './components/header/home/Hero';
+
+
+
+function App() {
+  return (
+    <>
+      <NavMenu/>
+      <Store/>
+   <Footer2/>
+   <Hero/>
+
+    </>
+
+
+  );
+}
+
+export default App;
